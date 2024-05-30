121ecfed00285e83e042f8f5cd1dafb4aff81c70
JaeHyeon <101800188+JaeHye0n-AI@users.noreply.github.com>
2024-05-30 20:29:25 +0900

    Add files via upload

---
+++ Model_ver_3/main_test.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+import os
+import librosa
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+import IPython
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torchvision.models import resnet18
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import scale
+import pandas as pd
+import glob
+import shutil
+import warnings
+import random
+import sys
+
+from datasets import *
+from train_test import *
+from TTA import *
+
+from options import *
+from config import *
+args = parse_args()
+config = Config(args)
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from utils import *
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]= "0"
+
+### experiment record ###
+study_name = "model_ver_3"
+study_path = f"{config.save_output_path}/{study_name}"
+
+if not os.path.exists(study_path):
+    os.makedirs(study_path)
+
+save_config(config, f'{config.save_output_path}/{study_name}/config.txt')
+
+### seed fix ###
+if config.seed >= 0:
+    set_seed(config.seed)
+    worker_init_fn = np.random.seed(config.seed)
+
+
+### train, val data prepare ###
+# train_loader, val_loader = mk_train_val_dataset(config.train_data_path, config.batch_size)
+train_loader = mk_train_val_dataset(config.train_data_path, config.batch_size)
+
+### model prepare ###
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = resnet18()
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+
+conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3)
+model.conv1 = conv1
+
+model = model.to(device)
+
+### start test ###
+test_df = pd.read_csv(config.submission_path)
+
+test_loader = mk_test_dataset(config.test_data_path)
+
+
+pretrained_model = f'{study_path}/best_model_ver_3.pt'
+if not pretrained_model == None:
+    model.load_state_dict(torch.load(pretrained_model)['model_state_dict'])
+
+optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=0)
+
+test_time_adap(model, test_loader, optimizer, device, study_path, test_df)
---
+++ Model_ver_3/options.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+
+def parse_args():
+    descript = 'Pytorch Implementation of \'fake_audio\''
+    parser = argparse.ArgumentParser(description=descript)
+
+    parser.add_argument('--train_data_path', default='../dataset/train')
+    parser.add_argument('--test_data_path', default='../dataset/test')
+    parser.add_argument('--submission_path', default='../dataset/submission.csv')
+
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--seed', type=int, default=4)
+    parser.add_argument('--num_iters', type=int, default=10)
+
+    parser.add_argument('--num_workers', type=int, default=4)
+
+    parser.add_argument('--save_output_path', type=str, default='./outputs')
+    return init_args(parser.parse_args())
+
+
+def init_args(args):
+    if not os.path.exists(args.save_output_path):
+        os.makedirs(args.save_output_path)
+
+    return args
